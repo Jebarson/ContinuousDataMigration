4d030fdb213f5e2d608d53769c1796e267cf48c7
Jebarson Jebamony <jebarson007@hotmail.com>
2017-07-12 13:11:09 +0530

    adding version to main project

--- AdventureWorks.Database/0.Scripts/PostDeployment/internal.DatabaseVersion.sql
+++ AdventureWorks.Database/0.Scripts/PostDeployment/internal.DatabaseVersion.sql
@@ -18,3 +18,24 @@
               'System' ,
               'System'
             );
+
+IF NOT EXISTS ( SELECT  1
+                FROM    [internal].[DatabaseVersion]
+                WHERE   [DatabaseVersionId] = 2 )
+    INSERT  INTO [internal].[DatabaseVersion]
+            ( [internal].[DatabaseVersion].[DatabaseVersionId] ,
+              [internal].[DatabaseVersion].[Major] ,
+              [internal].[DatabaseVersion].[Minor] ,
+              [internal].[DatabaseVersion].[Build] ,
+              [internal].[DatabaseVersion].[Revision] ,
+              [internal].[DatabaseVersion].[CreatedBy] ,
+              [internal].[DatabaseVersion].[ModifiedBy]
+            )
+    VALUES  ( 1 ,
+              1 ,
+              1 ,
+              0128 ,
+              212 ,
+              'System' ,
+              'System'
+            );
